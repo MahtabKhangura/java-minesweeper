67d1d54bc96cd695c797c781aee1e8e64496dfa5
Edward Chen <kheddie@Edwards-MacBook-Pro.local>
2022-11-29 14:59:32 -0800

    Create and print board

--- PrintBoard.java
+++ PrintBoard.java
@@ -1,5 +1,33 @@
 public class PrintBoard {
+    static String[][] board = new String[20][15];
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        // create_board();
+        // print_board();
+    }
+    public static void create_board() {
+        for (int x=0; x<20; x++) {
+            for (int y=0; y<15; y++) {
+                board[x][y] = "~~";
+            }
+        }
+    }
+    public static void print_board() {
+        System.out.println();
+        System.out.println("    1  2  3  4  5  6  7  8  9  10 11 12 13 14 15");
+        for (int x=0; x<20; x++) {
+            System.out.print(" ");
+            if (x<9) {
+                System.out.print(" " + (x+1));
+            } else {
+                System.out.print((x+1));
+            }
+            for (int y=0; y<15; y++) {
+                System.out.print(" " + board[x][y]);
+            }
+            System.out.print(" " + (x+1));
+            System.out.println();
+        }
+        System.out.println("    1  2  3  4  5  6  7  8  9  10 11 12 13 14 15");
+        System.out.println();
     }
 }
